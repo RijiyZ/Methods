a42780028c117b498003fd517d60880b66626db5
RijiyZ <ivandikarev3000@gmail.com>
2022-11-19 18:40:35 +0300

    ***

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.time.LocalDate;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void calculationYear() {
+        int year = 1488;
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " Является високосным годом");
+        } else {
+            System.out.println(year + " Не я вляется високосным годом");
+        }
     }
-}
+
+    public static void calculationYearOc() {
+        int currentYear = LocalDate.now().getYear();
+        int typeOc = 1;
+        int yearDevice = 2012;
+        if (currentYear > yearDevice && typeOc == 1){
+            System.out.println("Установите lite-версию для Android");
+        } if (currentYear > yearDevice && typeOc == 0) {
+            System.out.println("Установите lite-версию для ios");
+        }
+
+    }
+    public static void calculationDelivery(int deliveryDistance) {
+        int deliveryTime;
+        deliveryTime = deliveryDistance / 40;
+        deliveryTime ++;
+        System.out.println("Потребуется дней: " + deliveryTime);
+    }
+
+    public static void main(String[]args){
+        int deliveryDistance = 60;
+        calculationYear();
+        calculationYearOc();
+        calculationDelivery(deliveryDistance);
+        }
+    }
